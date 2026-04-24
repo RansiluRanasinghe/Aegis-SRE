61a9ea868e084504ebb7d50e002e33884afd641f
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-24 15:56:17 +0530

    feat: implemented endpoint to return the anomaly prediction

--- Backend/app/api/routes.py
+++ Backend/app/api/routes.py
@@ -11,3 +11,23 @@ async def health_check():
         "engine" : "Aegis-SRE Isolation Forest",
         "ready" : ml_engine.model is not None
     }
+
+@router.post("/analyze", response_model=AnomalyResonse, summary="Analyze Log Features", description="Endpoint to analyze log features and determine if it's an anomaly.")
+async def analyze_log(log_data: LogFeatureInput):
+
+    try:
+
+        prediction_result = ml_engine.predict(log_data.model_dump())
+
+        if prediction_result["is_anomaly"]:
+            msg = "CRITICAL: Outlier behavior detected."
+        else:
+            msg = "OK: Traffic within normal parameters." 
+
+        return AnomalyResonse(
+            is_anomaly=prediction_result["is_anomaly"],
+            confidence_score=prediction_result["confidence_score"],
+            message=msg
+         )
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))        
