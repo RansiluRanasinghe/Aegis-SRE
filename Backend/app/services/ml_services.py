34c105493d45cbd7c96863cfaada9f052b6ef7aa
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-24 14:33:29 +0530

    feat: implemented the predict method for predict the anomaly score

--- Backend/app/services/ml_services.py
+++ Backend/app/services/ml_services.py
@@ -17,7 +17,27 @@ class AegisMlService:
         checkpoint = joblib.load(model_path)
         self.model = checkpoint['model']
         self.scaler = checkpoint['scaler']
-        self.features = checkpoint['features']
+        self.feature_names = checkpoint['features']
 
         print("Aegis SRE: Model loaded successfully.")
+
+    def predict(self, log_data:dict) -> dict:
+
+        try:
+            ordered_features = [log_data[feature] for feature in self.feature_names]
+        except KeyError as e:
+            raise ValueError(f"Aegis SRE: Missing required feature in input data: {e}")
+
+        input_df = pd.DataFrame([ordered_features], columns=self.feature_names)
+
+        scaled_data = self.scaler.transform(input_df)
+        prediction = self.model.predict(scaled_data)[0]
+
+        score = self.model.predict_proba(scaled_data)[1]
+        is_anomaly = True if prediction == -1 else False
+
+        return {
+            "is_anomaly" : is_anomaly,
+            "confidence_score" : float(score)
+        }        
         
