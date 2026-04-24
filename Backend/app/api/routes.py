e1b21a16091e7a0389136411c89026e8b8d25047
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-24 15:51:09 +0530

    feat: implemented endpoint to check the API Health

--- Backend/app/api/routes.py
+++ Backend/app/api/routes.py
@@ -3,3 +3,11 @@ from app.schemas.log_schemas import LogFeatureInput, AnomalyResonse
 from app.services.ml_services import ml_engine
 
 router = APIRouter()
+
+@router.get("/health", summary="Health Check", description="Endpoint to check if the API is running.")
+async def health_check():
+    return {
+        "status" : "online",
+        "engine" : "Aegis-SRE Isolation Forest",
+        "ready" : ml_engine.model is not None
+    }
