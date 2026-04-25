7e7bd48941d44d4953ee168ca9b05d50bb236f3c
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-25 18:32:56 +0530

    feat: loaded the samlple logs data

--- Backend/simulate_traffic.py
+++ Backend/simulate_traffic.py
@@ -5,3 +5,13 @@ import sys
 
 API_URL = "http://localhost:8000/api/v1/analyze"
 DATA_PATH = "data/sample_logs.csv"
+
+def simulate():
+
+    print("Loading Data from, ", DATA_PATH)
+
+    try:
+        df = pd.read_csv(DATA_PATH)
+    except FileNotFoundError:
+        print("Error: sample_logs.csv not found in the data/ directory.")
+        sys.exit(1)
