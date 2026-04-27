ad1b9a5d88d6d29d46017f84dfff0be53cf4d091
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-27 15:40:14 +0530

    feat: updated /analyze response to handle llm diganosis

--- Backend/app/api/routes.py
+++ Backend/app/api/routes.py
@@ -17,22 +17,40 @@ async def health_check():
         "context_buffer_size" : len(context_buffer)
     }
 
-@router.post("/analyze", response_model=AnomalyResonse, summary="Analyze Log Features", description="Endpoint to analyze log features and determine if it's an anomaly.")
+@router.post("/analyze", response_model=AnomalyResonse, summary="Analyze Log & Trigger GenAI", description="Endpoint to analyze log features and determine if it's an anomaly.")
 async def analyze_log(log_data: LogFeatureInput):
 
     try:
 
-        prediction_result = ml_engine.predict(log_data.model_dump())
+        log_dict = log_data.model_dump()
 
-        if prediction_result["is_anomaly"]:
-            msg = "CRITICAL: Outlier behavior detected."
+        prediction_result = ml_engine.predict(log_dict)
+
+        if not prediction_result["is_anomaly"]:
+            context_buffer.append(log_dict)
+
+            return AnomalyResonse(
+                is_anomaly=False,
+                confidence_score=prediction_result["confidence_score"],
+                message="OK: Traffic within normal parameters.",
+                llm_diagnosis=None
+            )
+
         else:
-            msg = "OK: Traffic within normal parameters." 
 
-        return AnomalyResonse(
-            is_anomaly=prediction_result["is_anomaly"],
-            confidence_score=prediction_result["confidence_score"],
-            message=msg
-         )
+             diagnosis = llm_engine.diagnose(
+                 anomaly_log=log_dict,
+                    context_logs=list(context_buffer)
+             )
+
+             context_buffer.clear()
+
+             return AnomalyResonse(
+                is_anomaly=True,
+                confidence_score=prediction_result["confidence_score"],
+                message="CRITICAL: Outlier behavior detected. GenAI RCA generated.",
+                llm_diagnosis=diagnosis
+             )
+
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))        
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")       
