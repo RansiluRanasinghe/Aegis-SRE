f9bf0a542588fcbbc1bd311647c84365c8a03583
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-27 15:19:47 +0530

    feat: implemented the diagnose method to Takes the anomaouls log and make a root cause analysis

--- Backend/app/services/llm_service.py
+++ Backend/app/services/llm_service.py
@@ -8,3 +8,57 @@ class AegisLLMService:
         self.model = settings.LLM_MODEL
 
         print(f"Aegis-SRE: GenAI Engine initialized. Target: {self.api_url} ({self.model})")
+
+    def diagnose(self, anomaly_log: dict, context_logs: list[dict]) -> str:
+
+        print("Aegis-Brain: Crafting Root Cause Analysis...")
+
+        context_str = "\n".join([
+            f"- Status: {log['status']} | Bytes: {log['bytes']} | IP Freq: {log['ip_freq']}" 
+            for log in context_logs
+        ])
+
+        prompt =  f"""You are Aegis, an expert DevOps Site Reliability Engineer (SRE).
+        Your ML Sentinel has just flagged a critical anomaly in the server logs.
+
+        [RECENT NORMAL TRAFFIC]
+        {context_str if context_str else "No recent context available. Server just booted."}
+
+        [CRITICAL ANOMALY LOG]
+        Status Code: {anomaly_log['status']}
+        Payload Size (Bytes): {anomaly_log['bytes']}
+        Requests from this IP: {anomaly_log['ip_freq']}
+
+        [TASK]
+        Based purely on the data above:
+        1. Identify the likely technical root cause of this anomaly (e.g., DDoS, memory leak, broken endpoint, scanner bot).
+        2. Suggest one specific, actionable terminal command or engineering step to mitigate it.
+
+        [CONSTRAINTS]
+        Keep your answer under 3 sentences. Be highly technical, cold, and precise. Do not use pleasantries. Do not guess beyond the provided data.
+  """   
+        
+        payload = {
+            "model" : self.model,
+            "prompt" : prompt,
+            "stream" : False,
+            "options" : {
+                "temperature" : settings.LLM_TEMPERATURE,
+                "max_tokens" : settings.LLM_MAX_TOKENS
+            }
+        }
+
+        try:
+
+            response = requests.post(self.api_url, json=payload, timeout=10.0)
+            response.raise_for_status()
+
+            result = response.json()
+            return result.get("response", "Error: Empty response from LLM.").strip()
+        
+        except requests.exceptions.Timeout:
+            return "DIAGNOSIS FAILED: GenAI model timed out (Exceeded 10s). Manual SRE intervention required."
+        except requests.exceptions.ConnectionError:
+            return "DIAGNOSIS FAILED: Cannot connect to Ollama. Ensure Llama 3.2 is running locally on port 11434."
+        except Exception as e:
+            return f"DIAGNOSIS FAILED: Unexpected GenAI pipeline error: {str(e)}"
