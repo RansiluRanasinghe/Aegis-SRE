7763091cd2093031c4283aeb043f7a44400ac22b
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-05-19 15:37:42 +0530

    feat: removed simulator variables and Traffic control panel to give frontend live ears

--- Frontend/app.py
+++ Frontend/app.py
@@ -34,66 +34,7 @@ with st.sidebar:
         st.error("Backend: Offline")
         st.caption("Ensure Uvicorn is running on port 8000.")
 
-st.title("Live Telemetry & Diagnostics")
-st.markdown("Monitoring incoming API traffic for statistical anomalies...")
 
-if "log_history" not in st.session_state:
-    st.session_state.log_history = []
-if "activate_incident" not in st.session_state:
-    st.session_state.activate_incident = None
-
-st.subheader("Traffic Control Panel")
-st.caption("Simulate real-time log ingestion.")
-
-col1, col2, col3, col4 = st.columns(4)
-
-def send_traffic(payload, label):
-
-    with st.spinner(f"Ingesting {label}..."):
-
-        try:
-            res = requests.post(f"{API_BASE_URL}/analyze", json=payload, timeout=60)
-            if res.status_code == 200:
-                data = res.json()
-
-                new_log = {
-                    "Timestamp": time.strftime("%H:%M:%S"),
-                    "Type": label,
-                    "Status": payload["status"],
-                    "Bytes": payload["bytes"],
-                    "Freq": payload["ip_freq"],
-                    "ML Score": round(data["confidence_score"], 3),
-                    "Is Anomaly": "YES" if data["is_anomaly"] else "NO"
-                }
-
-                st.session_state.log_history.insert(0, new_log)
-                st.session_state.log_history = st.session_state.log_history[:10]
-
-                if data["is_anomaly"]:
-                    st.session_state.activate_incident = data
-                else:
-                    st.session_state.activate_incident = None
-
-        except requests.exceptions.ConnectionError:
-            st.error("Failed to connect to backend API.")
-
-with col1:
-    if st.button("Normal Traffic", width="stretch"):
-        send_traffic({"bytes": random.randint(500, 5000), "status": 200, "hour": 12, "ip_freq": random.randint(1, 10), "is_error": 0}, "Normal")
-
-with col2:
-    if st.button("Brute Force", width="stretch"):
-        send_traffic({"bytes": 120.0, "status": 401, "hour": 2, "ip_freq": 850, "is_error": 1}, "Brute Force")
-
-with col3:
-    if st.button("Exfiltration", width="stretch"):
-        send_traffic({"bytes": 5000000000.0, "status": 200, "hour": 14, "ip_freq": 2, "is_error": 0}, "Exfiltration")
-
-with col4:
-    if st.button("Cache Crash", width="stretch"):
-        send_traffic({"bytes": 0.0, "status": 502, "hour": 14, "ip_freq": 12, "is_error": 1}, "Crash Loop")
-
-st.markdown("---")
 
 feed_col, ai_col = st.columns([1.5, 1])
 
