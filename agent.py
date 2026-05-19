5bb68bd5cabd3b2b252cb734323a518a155ed57d
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-05-19 16:33:45 +0530

    feat: implemented the traffic injection algorithm

--- agent.py
+++ agent.py
@@ -8,3 +8,24 @@ print("Aegis Background Agent Initialized.")
 print("Simulating live server traffic. Press Ctrl+C to stop.\n")
 
 counter = 0
+
+while True:
+    counter += 1
+
+    if counter % 12 == 0:
+        print("INJECTING ANOMALY: Brute Force Attack!")
+        payload = {"bytes": 120.0, "status": 401, "hour": 2, "ip_freq": 850, "is_error": 1}
+
+    elif counter % 25 == 0:
+        print("INJECTING ANOMALY: Cache Crash!")
+        payload = {"bytes": 0.0, "status": 502, "hour": 14, "ip_freq": 12, "is_error": 1}
+
+    else:
+        print("Sending normal web traffic...")
+        payload = {
+            "bytes": random.randint(500, 5000), 
+            "status": 200, 
+            "hour": random.randint(8, 18), 
+            "ip_freq": random.randint(1, 10), 
+            "is_error": 0
+        }        
